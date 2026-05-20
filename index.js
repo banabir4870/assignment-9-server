48720ad537090ed839fd1b6167760ba831249a51
banabir4870 <banabir4870@gmail.com>
2026-05-21 04:58:04 +0600

    added token for booking and booking cancelletion

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const dotenv = require('dotenv')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config()
 const uri = process.env.MONGO_DB_URI;
 const app = express()
@@ -21,86 +22,112 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    await client.connect();
 
-    const db = client.db('drivefleet')
-    const carsCollection = db.collection('all-cars')
-    const bookingCollection = db.collection('bookings')
+const JWKS = createRemoteJWKSet(
+  new URL('http://localhost:3000/api/auth/jwks')
+)
 
-    app.get('/all-car', async (req, res) => {
-      const result = await carsCollection.find().toArray()
-      res.json(result)
-    })
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization
+  if(!authHeader){
+    return res.status(401).json({message: "Unauthorized"})
+  }
+  const token = authHeader.split(" ")[1]
+  if(!token){
+    return res.status(401).json({message: "Unauthorized"})
+  }
 
-    app.post('/all-car', async (req, res) => {
-      const carData = req.body;
-      const result = await carsCollection.insertOne(carData);
-      res.json(result);
-    })
-
-    
-
-    app.get('/all-car/user/:email', async (req, res) => {
-      const { email } = req.params
-      const result = await carsCollection.find({owner_email: email}).toArray()
-      res.json(result)
-    })
-
-    app.get('/all-car/:id', async (req, res) => {
-      const { id } = await req.params
-      const result = await carsCollection.findOne({ _id: new ObjectId(id) })
-      res.json(result)
-    })
-
-    app.patch('/all-car/:id', async (req, res) => {
-      const { id } = req.params
-      const updatedData = req.body
-      const result = await carsCollection.updateOne({ _id: new ObjectId(id) }, { $set: updatedData })
-      res.json(result)
-    })
-
-    app.delete('/all-car/:id', async(req, res)=>{
-      const {id} = req.params;
-      const result = await carsCollection.deleteOne({_id: new ObjectId(id)})
-      res.json(result)
-    })
-
-    app.get('/featured', async (req, res) => {
-      const result = await carsCollection.find({ availability: true }).limit(6).toArray()
-      res.json(result)
-    })
-
-    app.get('/booking/:userId', async (req, res)=>{
-      const { userId } = req.params
-      const result = await bookingCollection.find({ userId }).toArray();
-      res.json(result)
-
-    })
-
-
-    app.post('/booking', async (req, res) => {
-      const bookingData = req.body
-      const result = await bookingCollection.insertOne(bookingData)
-      res.json(result)
-    })
-
-
-    app.delete('/booking/:bookingId', async(req, res)=>{
-      const {bookingId} = req.params
-      const result = await bookingCollection.deleteOne({_id: new ObjectId(bookingId)})
-      res.json(result)
-    })
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  try{
+    const {payload} = await jwtVerify(token, JWKS)
+    console.log(payload)
+    next()
+  }catch(error){
+    return res.status(403).json({message: "Forbidden"})
   }
 }
+
+  async function run() {
+    try {
+
+
+      await client.connect();
+
+      const db = client.db('drivefleet')
+      const carsCollection = db.collection('all-cars')
+      const bookingCollection = db.collection('bookings')
+
+      app.get('/all-car', async (req, res) => {
+        const result = await carsCollection.find().toArray()
+        res.json(result)
+      })
+
+      app.post('/all-car', async (req, res) => {
+        const carData = req.body;
+        const result = await carsCollection.insertOne(carData);
+        res.json(result);
+      })
+
+
+
+      app.get('/all-car/user/:email', async (req, res) => {
+        const { email } = req.params
+        const result = await carsCollection.find({ owner_email: email }).toArray()
+        res.json(result)
+      })
+
+      app.get('/all-car/:id', verifyToken,async (req, res) => {
+        const { id } = await req.params
+        const result = await carsCollection.findOne({ _id: new ObjectId(id) })
+        res.json(result)
+      })
+
+      app.patch('/all-car/:id', async (req, res) => {
+        const { id } = req.params
+        const updatedData = req.body
+        const result = await carsCollection.updateOne({ _id: new ObjectId(id) }, { $set: updatedData })
+        res.json(result)
+      })
+
+      app.delete('/all-car/:id', async (req, res) => {
+        const { id } = req.params;
+        const result = await carsCollection.deleteOne({ _id: new ObjectId(id) })
+        res.json(result)
+      })
+
+      app.get('/featured', async (req, res) => {
+        const result = await carsCollection.find({ availability: true }).limit(6).toArray()
+        res.json(result)
+      })
+
+      app.get('/booking/:userId', verifyToken,async (req, res) => {
+        const { userId } = req.params
+        const result = await bookingCollection.find({ userId }).toArray();
+        res.json(result)
+
+      })
+
+
+      app.post('/booking', verifyToken,async (req, res) => {
+        const bookingData = req.body
+        const result = await bookingCollection.insertOne(bookingData)
+        res.json(result)
+      })
+
+
+      app.delete('/booking/:bookingId', verifyToken,async (req, res) => {
+        const { bookingId } = req.params
+        const result = await bookingCollection.deleteOne({ _id: new ObjectId(bookingId) })
+        res.json(result)
+      })
+
+
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+    }
+  }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
